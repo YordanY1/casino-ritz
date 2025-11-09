45a6e27bc657cc3c90d793460bc9f2b0bab8d680
YordanYordanov <yordanyordanov0104@gmail.com>
2025-11-09 21:23:17 +0200

    Update CreatePhoto.php

--- app/Filament/Resources/Photos/Pages/CreatePhoto.php
+++ app/Filament/Resources/Photos/Pages/CreatePhoto.php
@@ -3,9 +3,33 @@
 namespace App\Filament\Resources\Photos\Pages;
 
 use App\Filament\Resources\Photos\PhotoResource;
+use App\Models\Photo;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePhoto extends CreateRecord
 {
     protected static string $resource = PhotoResource::class;
+
+    protected function handleRecordCreation(array $data): Photo
+    {
+        if (is_array($data['path'])) {
+            $albumId = $data['album_id'];
+            $first = null;
+
+            foreach ($data['path'] as $path) {
+                $photo = Photo::create([
+                    'album_id' => $albumId,
+                    'path' => $path,
+                ]);
+
+                if (!$first) {
+                    $first = $photo;
+                }
+            }
+
+            return $first;
+        }
+
+        return Photo::create($data);
+    }
 }
